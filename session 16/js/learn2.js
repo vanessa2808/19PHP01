6d5f3754913903fad367144c50c364779689a220
vanessa2808 <yenrion9941@gmail.com>
2019-04-21 17:09:07 +0700

    all existing files

--- session 16/js/learn2.js
+++ session 16/js/learn2.js
@@ -18,10 +18,38 @@ $('.runImage').click(function(){
 	$('#myBox1').animate({'left':'10px'});
 	$('#myBox1').animate({'top':'400px'});
 });
-$('.runImage1').click(function(){
-	$('#myBox2').animate({'top':'300px'});
+$('.runImage2').click(function(){
+	$('#runImage3').animate({'top':'300px'});
+	$('#runImage3').animate({'right':'100px'});
+	$('#runImage3').animate({'right':'50px'});
+	$('#runImage3').animate({'top':'400px'});
+	$('#runImage3').animate({'right':'500px'});
+});
+$('.runImage').click(function(){
+	$('#myBox1').animate({'right':'200px'});
+	$('#myBox1').animate({'left':'600px'});
+	$('#myBox1').animate({'right':'50px'});
+	$('#myBox1').animate({'top':'400px'});
+
+	$('#myBox2').animate({'top':'600px'});
 	$('#myBox2').animate({'right':'100px'});
-	$('#myBox2').animate({'right':'50px'});
-	$('#myBox2').animate({'top':'400px'});
-	$('#myBox2').animate({'right':'500px'});
+	$('#myBox2').animate({'left':'10px'});
+
+	$('#myBox3').animate({'top':'900px'});
+	$('#myBox3').animate({'right':'100px'});
+	$('#myBox3').animate({'left':'600px'});
+
+	$('#myBox4').animate({'right':'900px'});
+	$('#myBox4').animate({'top':'300px'});
+	$('#myBox4').animate({'left':'600px'});
+
+	$('#myBox5').animate({'right':'200px'});
+	$('#myBox5').animate({'left':'600px'});
+	$('#myBox5').animate({'left':'10px'});
+
+
+	$('#myBox6').animate({'right':'200px'});
+	$('#myBox6').animate({'left':'600px'});
+	$('#myBox6').animate({'top':'400px'});
+
 });
