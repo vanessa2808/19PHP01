a27019f41eeff49e2339c9e56618411a34358e7c
vanessa2808 <yenrion9941@gmail.com>
2019-04-26 18:05:29 +0700

    all existing files

--- session 16/js/learn1.js
+++ session 16/js/learn1.js
@@ -1,84 +1,17 @@
 function checkEmpty() {
 	var name = document.getElementById('displayName').value;
-	var day = document.getElementById('date').value;
-	var gender = document.getElementById('displayGender').value;
+	var day = document.getElementById('displayDate').value;
+	var gender = document.getElementById('displayGender').checked;
+	var male = document.getElementById('displayGender').checked;
+	var female = document.getElementById('displayGender').checked;
 	var phone = document.getElementById('displayNumber').value;
-	var month = document.getElementById('month').value;
-	var year = document.getElementById('year').value;
-	if (name == '') {
-		$('#displaySubmit').click(function(){
-			$('#errorName').html('please enter name!');
-		});
-	} else {
-		$('#displaySubmit').click(function(){
-			$('#errorName').html('');
-		});
+	var date = new Date(day);
+	year = date.getFullYear();
+	var phone = document.getElementById('displayNumber')
+	if(name == ''){
+		alert('please, enter name');
+	} else if (day == ''){
+		alert('please enter day of birth')
 	}
-	if (phone == '') {
-		$('#displaySubmit').click(function(){
-			$('#errorPhone').html('please enter your phone!');
-		});
-
-	} else {
-		$('#displaySubmit').click(function(){
-			$('#errorName').html('');
-		});
-	}
-	if(day == ''){
-		$('#displaySubmit').click(function(){
-			$('#errorDay').html('please enter day!');
-		});
-	}
-	else {
-		if (day <= 0 || day > 31){
-			$('#displaySubmit').click(function(){
-			$('#errorName').html('[error:]Invalid number');
-		});
-		} else {
-			document.getElementById('errorYear').innerHTML ='';
-		}
-	}
-	if(month == ''){
-		document.getElementById('errorMonth').innerHTML = 'please enter month';
-		document.getElementById('errorMonth').style.fontSize = "12px";
-	}
-	else {
-		if (month <= 0 || month > 12){
-			document.getElementById('negativeDate').innerHTML ='[error:]Invalid number';
-			document.getElementById('negativeDate').style.fontSize = "12px";
-			return '';
-		} else {
-			document.getElementById('errorMonth').innerHTML ='';
-		}
-	}
-	if(year == ''){
-		document.getElementById('errorYear').innerHTML = 'please enter year';
-		document.getElementById('errorYear').style.fontSize = "12px";
-	}
-	else {
-		document.getElementById('errorYear').innerHTML ='';
-		if (year <= 0){
-			document.getElementById('negativeDate').innerHTML ='[error:]Invalid number';
-			document.getElementById('negativeDate').style.fontSize = "12px";
-			return '';
-		} else {
-			document.getElementById('errorYear').innerHTML ='';
-		}
-	}
-	if (phone != '' && name != ''&& day != '' && month != '' && year !=''){
-		document.getElementById('rate').innerHTML = 'Sucessfully';
-		if (parseInt(year) > 1998){
-			document.getElementById('display1').innerHTML = "Name: "+name;
-			document.getElementById('display1').style.color = 'red';
-			document.getElementById('display2').innerHTML = "Gender: "+ gender;
-			document.getElementById('display3').innerHTML = "date Of Birth: "+ day + "/" +month + "/"+ year; 
-			document.getElementById('display4').innerHTML = "Phone: "+ phone;
-		} else {
-			document.getElementById('display1').innerHTML = "Name: "+name;
-			document.getElementById('display1').style.color = 'green';
-			document.getElementById('display2').innerHTML = "Gender: "+ gender;
-			document.getElementById('display3').innerHTML = "date Of Birth: "+ day + "/" +month + "/"+ year; 
-			document.getElementById('display4').innerHTML = "Phone: "+ phone;
- 		}
-	}
+	else if()
 }
