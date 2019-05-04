ced6d976b075882b88e7848c0b1e8d95fcd7f2d4
vanessa2808 <yenrion9941@gmail.com>
2019-05-04 13:34:18 +0700

    all existing files

--- session 18/js/excercise.js
+++ session 18/js/excercise.js
@@ -33,9 +33,12 @@ function checkForm() {
 	if (last == '') {
 		document.getElementById('errorLast').innerHTML = "please enter last day";
 		document.getElementById('errorLast').style.color = "red";
-	} else {
+	} else if (last < first){
+			document.getElementById('errorLast').innerHTML = "The last day must be more than the first day!";
+			document.getElementById('errorLast').style.color = "red";
+		} else {
 		document.getElementById('errorLast').innerHTML ='';
-	}
+		}
 	if (firstNumber == '') {
 		document.getElementById('errorNumber1').innerHTML = "please enter first Number";
 		document.getElementById('errorNumber1').style.color = "red";
@@ -45,36 +48,43 @@ function checkForm() {
 	if (lastNumber == '') {
 		document.getElementById('errorNumber2').innerHTML = "please enter last Number";
 		document.getElementById('errorNumber2').style.color = "red";
-	} else {
-		document.getElementById('errorNumber2').innerHTML ='';
-	}
-	if(name != '' && phone != '' && address != '' && last!='' && first!= '' && firstNumber!='' && lastNumber != ''){
-		//document.getElementById('display1').innerHTML = 'sucessfully'
-		alert('sucessfully!');
-	}
-	if(lastNumber < firstNumber){
+	} else if(lastNumber < firstNumber){
 			document.getElementById('errorNumber2').innerHTML = "please enter invalid last Number";
 			document.getElementById('errorNumber2').style.color = "red";
+		} else {
+		document.getElementById('errorNumber2').innerHTML ='';
 	}
-	// document.write("<br/>");
-	// document.getElementById('display') = name;
-	// document.getElementById('display') = address;
-	// document.getElementById('display') = phone;
-	// document.getElementById('display') = first;
-	// document.getElementById('display') = last;
-	// document.getElementById('display') = firstNumber;
-	// document.getElementById('display') = lastNumber;
-	var sum = (lastNumber) - (firstNumber);
-	if(sum <= 100) {
-		sum = sum * 1500;
-		document.getElementById('display').innerHTML ="The cost of electricity: " + sum;
+	if(name != '' && phone != '' && address != '' && last!='' && first != '' && firstNumber!='' && lastNumber != '' && (first < last) && (firstNumber < lastNumber)){
+		document.getElementById('success').innerHTML = 'Sucessfully! '
+	} else {
+		document.getElementById('success').innerHTML = '';
 	}
-	else if(sum < 100 && sum < 300){
-		sum = 100*1500 + (sum - 100)*2000;
-		document.getElementById('display').innerHTML ="The cost of electricity: " + sum;
-	} else{
-	 	 sum = 100*1500 + 200*2000 +(sum-300)*3000;
-		document.getElementById('display').innerHTML ="The cost of electricity: " + sum;
-	}
+}
+function calculateEle(){
+	var firstNumber = document.getElementById('number1').value;
+	var lastNumber = document.getElementById('number2').value;
+	var sum;
+	sum = parseInt(lastNumber) - parseInt(firstNumber);
+	if (sum <= 0) document.getElementById('display9') = 'The cost is invalid';
+	else if (sum <= 100) var sum1 = sum * 1500;
+	else if (sum > 100 && sum <= 300) var sum1 = 100 * 1500 + (sum - 100) * 2000;
+	else if(sum > 300) var sum1 = 100 * 1500 + 200 * 2000 + (sum - 200) * 3000;
+		document.getElementById('display9').innerHTML = '<br> The cost of electricity = ' + sum1;
+}
+function printPersonalInformation(){
+	var name = document.getElementById('name').value;
+	var address = document.getElementById('address').value;
+	var phone = document.getElementById('phone').value;
+	var first = document.getElementById('first').value;
+	var last = document.getElementById('last').value;
+	var firstNumber = document.getElementById('number1').value;
+	var lastNumber = document.getElementById('number2').value;
+	document.getElementById('display2').innerHTML = "Name: "+name;
+	document.getElementById('display3').innerHTML = "Address: " + address;
+	document.getElementById('display4').innerHTML = "Phone: "+phone;
+	document.getElementById('display5').innerHTML = "First day: "+first;
+	document.getElementById('display6').innerHTML = "Last day: "+last;
+	document.getElementById('display7').innerHTML = "First Number: "+firstNumber;
+	document.getElementById('display8').innerHTML = "Last Number: "+lastNumber;
 }
 
